f1b59b841ee2d078d88797b7d21acc9efa94aba8
navneet0217 <navneetanand17360@gmail.com>
2026-09-21 10:10:24 +0000

    Fix frontend API URL for production

--- frontend/src/app/todo.service.ts
+++ frontend/src/app/todo.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Todo } from './todo.model';
 
-const API_URL = 'http://localhost:8000/todos';
+const API_URL = '/api/todos';
 
 @Injectable({ providedIn: 'root' })
 export class TodoService {
